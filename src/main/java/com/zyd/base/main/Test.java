1109556a3deebd42169b434e9e7e73e5036c38b0
zhongyuandong <zhongyuandong@zj.tech>
2023-03-09 12:05:01 +0800

    初始化

--- src/main/java/com/zyd/base/main/Test.java
+++ src/main/java/com/zyd/base/main/Test.java
@@ -5,6 +5,7 @@ import com.zyd.base.utils.StringCompressUtils;
 public class Test {
 
     public static void main(String[] args) {
+        System.out.println("开始执行压缩....");
         String str = "sfsdafdsadfasdfsfsdafdsadfasdfsfsdafdsadfasdfsfsdafdsadfasdfsfsdafdsadfasdfsfsdafdsadfasdfsfsdafdsadfasdfsfsdafdsadfasdfsfsdafdsadfasdfsfsdafdsadfasdf";
         System.out.println("压缩前：" + str);
         String compress = StringCompressUtils.compress(str);
